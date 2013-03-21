abef6494af6f2e00435175f45cfdb5da524de20e
duchesnay <edouard.duchesnay@gmail.com>
2013-03-21 19:44:10 +0100

    add sklearn_plugins/__init__.py

---
+++ sklearn_plugins/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 15:45:21 2013
+
+@author: edouard.duchesnay@cea.fr
+"""
+
+from .resampling import Permutation
+#import sklearn_plugins
+
+__all__ = ['Permutation']
