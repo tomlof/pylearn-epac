45c5a0628791d33225a56004ac8801927623447e
jinpeng <mr.li.jinpeng@gmail.com>
2013-07-19 15:08:20 +0200

    DOC: upload script

---
+++ doc/source/upload2github.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script has only been test on ubuntu
+
+make html
+
+outdir="$(mktemp -d)"
+curdir="$(pwd)"
+
+git clone git@github.com:neurospin/pylearn-epac.git $outdir
+
+cd $outdir
+git fetch origin
+git checkout -b gh-pages origin/gh-pages
+
+cp -r $curdir/_build/html/* $outdir
+
+git add .
+
+git commit -a -m "DOC: update pages"
+
+git push origin gh-pages
+
+cd $curdir
